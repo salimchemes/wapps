1207ddfcd15d97d9b50e6736d3aee14f5e775021
Joaquin Cid <joaqcid@gmail.com>
2017-01-31 14:47:23 -0300

    wip - add localization buttons

--- www/index.aspx.cs
+++ www/index.aspx.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 using System.Threading;
 
 
@@ -7,10 +8,17 @@ public partial class _Index : System.Web.UI.Page
     protected void Page_Load(object sender, EventArgs e)
     {
         if (Request.QueryString["language"] == "en")        
-            Thread.CurrentThread.CurrentUICulture = System.Globalization.CultureInfo.GetCultureInfo("en-US");
+            Thread.CurrentThread.CurrentUICulture = CultureInfo.GetCultureInfo("en-US");
+
         if (Request.QueryString["language"] == "es")
-            Thread.CurrentThread.CurrentUICulture = System.Globalization.CultureInfo.GetCultureInfo("es-AR");
+            Thread.CurrentThread.CurrentUICulture = CultureInfo.GetCultureInfo("es-AR");
         if (String.IsNullOrEmpty(Request.QueryString["language"]))
-            Thread.CurrentThread.CurrentUICulture = System.Globalization.CultureInfo.GetCultureInfo("es-AR");
+        {
+            Thread.CurrentThread.CurrentUICulture = CultureInfo.GetCultureInfo("es-AR");
+            Thread.CurrentThread.CurrentCulture = CultureInfo.GetCultureInfo("es-AR");
+
+            this.Culture = "es-AR";
+            this.UICulture = "es-AR";
+        }
     }
 }
